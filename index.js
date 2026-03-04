397239a84e1dc66458ee801a7d36a6f0ae7ff534
Imamsaa <imamsafii2222022@gmail.com>
2026-03-04 09:56:34 +0700

    feat : Connect Database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,10 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB:', err));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello, World!' });

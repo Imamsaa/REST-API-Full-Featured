da2ee2ed91dcdd63766f40df8b9e2601a78df7b9
Imamsaa <imamsafii2222022@gmail.com>
2026-03-13 00:20:30 +0700

    feat : Create fitur jwt signin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 import userModel from '../models/usersModel.js';
 import validator from '../middlewares/validator.js';
 import beHash from '../utils/hash.js';
-
+import jwt from 'jsonwebtoken';
 const signup = async (req, res) => {
     try {
         const { error } = validator.validateSignupSchema.validate(req.body);
@@ -16,7 +16,7 @@ const signup = async (req, res) => {
         const hashedPassword = await beHash.doHash(password);
         const newUser = new userModel({ email, password: hashedPassword });
         await newUser.save();
-        res.status(201).json({ message: 'User registered successfully' });
+        res.status(201).json({ message: 'User created successfully' });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Server error' });
@@ -31,7 +31,7 @@ const signin = async (req, res) => {
             return res.status(400).json({ error: error.details[0].message });
         }
         const { email, password } = req.body;
-        const user = await userModel.findOne({ email });
+        const user = await userModel.findOne({ email }).select('+password');
         if (!user) {
             return res.status(400).json({ error: 'Invalid email or password' });
         }
@@ -40,6 +40,12 @@ const signin = async (req, res) => {
         if (!isPasswordValid) {
             return res.status(400).json({ error: 'Invalid email or password' });
         }
+        const token = jwt.sign({ 
+            userId: user._id,
+            email: user.email,
+            password: user.password
+        }, process.env.TOKEN_SECRET);
+        res.cookie('Authorization', `Bearer ${token}`, { expires: new Date(Date.now() + 8 * 3600000), httpOnly: process.env.NODE_ENV === 'production', secure: process.env.NODE_ENV === 'production' });
         res.status(200).json({ message: 'Signin successful' });
     } catch (err) {
         console.error(err);
--- utils/hash.js
+++ utils/hash.js
@@ -1,4 +1,4 @@
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
 
 const doHash = async (password, saltRounds = 10) => {
     return await hash(password, saltRounds);
